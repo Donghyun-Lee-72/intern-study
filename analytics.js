41cf5adb52fa53b5eb18fa68b4e6bd0b69bf267d
Donghyun Lee <dhlee991124@gmail.com>
2026-09-01 12:54:33 +0900

    Restore privacy-limited GA4 analytics

---
+++ analytics.js
@@ -0,0 +1,19 @@
+(() => {
+  const measurementId = "G-6Z3WTNQ64Q";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag() {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId, {
+    allow_google_signals: false,
+    allow_ad_personalization_signals: false,
+  });
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+})();
